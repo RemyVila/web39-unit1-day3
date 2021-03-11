264ef07da20ec75149d3a60c75914e2f788ad4ee
Remy Vila <josephvila123@gmail.com>
2021-03-11 13:43:41 -0500

    trying again lol

--- index.js
+++ index.js
@@ -1,19 +1,24 @@
 require('dotenv').config()
 const path = require('path')
 const express = require('express')
-const server = express();
+const server = express()
 server.use(express.json())
-
-server.use(express.static(path.join(__dirname, 'client/build', 'index.html')))
-
-if(process.env.NODE_ENV !== 'production'){
+server.use(express.static(path.join(__dirname, 'client/build')))
+console.log(process.env.NODE_ENV)
+// on Heroku machine, an env variable is called "NODE_ENV" -> "production"
+if (process.env.NODE_ENV === 'development') {
   const cors = require('cors')
   server.use(cors())
 }
-server.use('*', (res,req) => {
+// our API comes earlier in the pipeline
+server.get('/api/hello', (req, res) => {
+  res.json({ message: 'hello' })
+})
+// catch-all that just sends back index.html
+server.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'client/build', 'index.html'))
 })
 const PORT = process.env.PORT || 4000
 server.listen(PORT, () => {
-  console.log(`listing on ${PORT}`)
+  console.log(`listening on ${PORT}`)
 })
